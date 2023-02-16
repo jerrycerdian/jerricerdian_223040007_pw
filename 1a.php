a7d6753022c6c3217fb51814c3b245a154ec9256
jerrycerdian <distirajerrycerdian@gmail.com>
2023-02-16 12:41:12 +0700

    first commit

---
+++ 1a.php
@@ -0,0 +1,17 @@
+<?php 
+$x = "Topi";
+$y = "Bundar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><i><?php echo "\"$x saya $y, $y $x saya 
+    \"" ?></i></h1>
+</body>
+</html>
